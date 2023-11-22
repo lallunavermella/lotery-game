81e455f9c9c2466b771d4a4f026e64f30b7d8c2a
Silvia Martinez <silvia.martinez@Silvias-MacBook-Pro-2.local>
2023-11-22 14:23:23 +0100

    Create numbers store

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import { useNumbersStore } from './stores/numbers'
 
 const app = createApp(App)
 
 app.use(createPinia())
+export const numbersStore = useNumbersStore()
 
 app.mount('#app')
