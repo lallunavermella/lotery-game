f439a84505942268ff581ef316674b7f0698beed
Silvia Martinez <silvia.martinez@Silvias-MacBook-Pro-2.local>
2023-11-30 13:11:24 +0100

    Added new store to manage game result

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { useNumbersStore } from './stores/userNumbers'
 import { useLoteryNumbersStore } from './stores/loteryNumbers'
+import { useResultGameStore } from './stores/resultGame'
 
 const app = createApp(App)
 
 app.use(createPinia())
 export const numbersStore = useNumbersStore()
 export const loteryNumbersStore = useLoteryNumbersStore()
+export const resultsGameStore = useResultGameStore()
 
 app.mount('#app')
